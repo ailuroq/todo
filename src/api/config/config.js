0c29f625c26fd42467a18ebd572753053c0ed62e
ailuroq <yaroslav_ovsyannikov@list.ru>
2025-01-26 20:08:57 +0300

    fix config

--- src/api/config/config.js
+++ src/api/config/config.js
@@ -13,6 +13,7 @@ export default {
     port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     name: process.env.DB_NAME,
   },
   s3: {
